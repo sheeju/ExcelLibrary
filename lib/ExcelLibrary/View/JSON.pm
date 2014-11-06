a625dad655200f53ac87b407c4551f518a4a346a
skanda-shastry <skanda@exceleron.com>
2014-11-06 12:18:30 +0530

    Integration

---
+++ lib/ExcelLibrary/View/JSON.pm
@@ -0,0 +1,29 @@
+package ExcelLibrary::View::JSON;
+
+use strict;
+use base 'Catalyst::View::JSON';
+
+=head1 NAME
+
+ExcelLibrary::View::JSON - Catalyst JSON View
+
+=head1 SYNOPSIS
+
+See L<ExcelLibrary>
+
+=head1 DESCRIPTION
+
+Catalyst JSON View.
+
+=head1 AUTHOR
+
+skanda,,,
+
+=head1 LICENSE
+
+This library is free software, you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
