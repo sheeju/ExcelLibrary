49272b8877107b463dc18a864478176cb5bd163b
skanda-shastry <skanda@exceleron.com>
2014-11-18 10:45:37 +0530

    Default login

--- lib/ExcelLibrary/Controller/Root.pm
+++ lib/ExcelLibrary/Controller/Root.pm
@@ -29,10 +29,8 @@ The root page (/)
 =cut
 
 sub index :Path :Args(0) {
-    my ( $self, $c ) = @_;
-
-    # Hello World
-    $c->response->body( $c->welcome_message );
+	my ( $self, $c ) = @_;
+	$c->stash->{template} = 'login/index.tt';
 }
 
 =head2 default
