ba5268c9bcab24357191e931132bda9a9d5feb17
Pavan <pavan@exceleron.com>
2014-12-04 16:56:31 +0530

    migration of js

--- root/source/js/login/forgotpassword.js
+++ root/source/js/login/forgotpassword.js
@@ -1,37 +1,34 @@
 $(document).ready(function() {
-			$("#loginform").validate({
-				rules: {
-					email: {
-						required: true,
-						email: true
-					}
+    $("#loginform").validate({
+        rules: {
+            email: {
+                required: true,
+                email: true
+            }
 
-				},
-				submitHandler: function(form) {
-					var email =$('#user_email').val();
-					$.ajax({
-						url:'login/validate',
-						data:{
-							email:email,
-						}
-						}).done(function(data) {
-						if(data.responsemessage)
-						{
-							$('#failmessage').html("Email is not registered");
-							$('#user_email').val('');
-						}	
-						else
-						{
-							$('#donemessage').html("Check your Email for change password");
-							$('#user_email').val('');
-						}	
-					});
+        },
+        submitHandler: function(form) {
+            var email = $('#user_email').val();
+            $.ajax({
+                url: 'login/validate',
+                data: {
+                    email: email,
+                }
+            }).done(function(data) {
+                if (data.responsemessage) {
+                    $('#failmessage').html("Email is not registered");
+                    $('#user_email').val('');
+                } else {
+                    $('#donemessage').html("Check your Email for change password");
+                    $('#user_email').val('');
+                }
+            });
 
-				}
-			});
-			$( 'input' ).focus(function() {
-				$('#failmessage').html("");
-				$('#donemessage').html("");
-			});
+        }
+    });
+    $('input').focus(function() {
+        $('#failmessage').html("");
+        $('#donemessage').html("");
+    });
 
-			});
+});
