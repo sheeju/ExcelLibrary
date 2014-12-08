faecc45dac5f04c47fdf25b605cd2bb4278cfa04
VekatesanNarayanan <vekatesanmusiri@gmail.com>
2014-12-08 16:19:40 +0530

    empty records in history tab problem get resolved

--- root/source/js/dashboard/history.js
+++ root/source/js/dashboard/history.js
@@ -1,29 +1,30 @@
 $(document).ready(function() {
 	var table = $('#historyTable').dataTable();
-	var numberofcolumn =table.fnGetData(0).length;
-	if(numberofcolumn == 8)
+	var numberofrecords =table.fnSettings().aoData.length;
+	if(numberofrecords != 0)
 	{
+		var numberofcolumn = table.fnGetData(0).length;
+		if(numberofcolumn == 8)
+		{
 
-		table.yadcf([{
+			table.yadcf([{
 
-			column_number: 1
-		}, {
-			column_number: 2
-		}, {
-			column_number: 7
-		}]);
+				column_number: 1,filter_match_mode : "exact"
+			}, {
+				column_number: 2,filter_match_mode : "exact"
+			}, {
+				column_number: 7,filter_match_mode : "exact"
+			}]);
+		}
+		else
+		{
+			table.yadcf([{
+
+				column_number: 1,filter_match_mode : "exact"
+			}, {
+				column_number: 6,filter_match_mode : "exact"
+			}]);
+
+		}
 	}
-	else
-	{
-		table.yadcf([{
-
-			column_number: 1
-		}, {
-			column_number: 2
-		}, {
-			column_number: 6
-		}]);
-
-	}
-
 });
